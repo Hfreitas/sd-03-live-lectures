943260bb9673b4b00aaef7ae6317fe14cefbcc3a
Rogério Munhoz <roz@rjmunhoz.me>
2020-09-15 17:46:41 -0300

    Changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const SERVER_ENV = process.env.SERVER_ENV || 'deu ruim :/';
 
 app.get('/', (_, res) => {
-  res.send(`Você está no ambiente de ${SERVER_ENV}`);
+  res.send(`Você está navegando no ambiente de ${SERVER_ENV}`);
 });
 
 
