d8a3adbcbedb91442a577d8cda0c3ca562ca0037
Rogério Munhoz <roz@rjmunhoz.me>
2020-09-16 18:19:25 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const color = require('random-hex-color')();
+
+app.get('/', (_, res) => {
+  res.type('text/html').send(
+    `<body style="background-color: ${color}">
+      <h1>Aplicação rodando no processo: ${process.pid}!</h1>
+    </body>`
+  );
+});
+
+app.get('/bug', (_, res) => {
+  res.send('Um bug ocorreu! :O');
+
+  process.exit(1);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => { console.log(`Escutando na porta ${PORT}`); });
