8ac7458951d3e5e038065bc6ae112b56b3ebdce2
Rogério Munhoz <roz@rjmunhoz.me>
2020-09-16 18:24:03 -0300

    Add PORT environment variable

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.get('/bug', (_, res) => {
   process.exit(1);
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => { console.log(`Escutando na porta ${PORT}`); });
